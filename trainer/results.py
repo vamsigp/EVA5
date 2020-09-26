53542139debccf474ec71c7846c7d7ed9e34b797
Vamsi <vkrishna84@gmail.com>
2020-09-26 17:47:41 +0530

    added plot metrics
    added plot metrics

--- trainer/results.py
+++ trainer/results.py
@@ -25,3 +25,15 @@ def plot_graph(data, metric):
     plt.show()
     
     fig.savefig(f'val_%s_change.png' % (metric.lower()))
+    
+
+def plot_metrics(train_losses, train_acc, test_losses, test_acc):
+    fig, axs = plt.subplots(2,2,figsize=(15,10))
+    axs[0, 0].plot(train_losses)
+    axs[0, 0].set_title("Training Loss")
+    axs[1, 0].plot(train_acc)
+    axs[1, 0].set_title("Training Accuracy")
+    axs[0, 1].plot(test_losses)
+    axs[0, 1].set_title("Test Loss")
+    axs[1, 1].plot(test_acc)
+    axs[1, 1].set_title("Test Accuracy")
