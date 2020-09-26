9d4a1ed7aa14bd4b9f68e293b5108cada44e127a
Vamsi <vkrishna84@gmail.com>
2020-09-26 14:26:41 +0530

    update trainer.py class
    update trainer.py class

--- trainer/trainer.py
+++ trainer/trainer.py
@@ -7,46 +7,62 @@ from tqdm import tqdm
 
 class Trainer():
 
-  def __init__(self):
+  def __init__(self, model, device, train_loader, test_loader, optimizer, loss_func, lr_scheduler):
     self.train_losses = []
     self.test_losses = []
     self.train_acc = []
     self.test_acc = []
+    self.model = model
+	self.device = device
+	self.train_loader = train_loader
+	self.test_loader = test_loader
+	self.optimizer = optimizer
+	self.loss_func = loss_func
+	self.lr_scheduler = lr_scheduler
+	
+  def train_model(self, epoch = 5, lambda_l1):
+	for epoch in range(epochs):
+		print("EPOCH:", epoch+1)
+		train(epoch, lambda_l1)
+		test()
+	return (self.train_losses, self.train_acc, self.test_losses, self.test_acc)
+		
 
-  def train(self, model, device, train_loader, optimizer, epoch, lambda_l1):
-    model.train()
-    pbar = tqdm(train_loader)
+  def train(self, epoch, lambda_l1):
+    self.model.train()
+    pbar = tqdm(self.train_loader)
     correct = 0
     processed = 0
     for batch_idx, (data, target) in enumerate(pbar):
       # get samples
-      data, target = data.to(device), target.to(device)
+      data, target = data.to(self.device), target.to(self.device)
 
       # Init
-      optimizer.zero_grad()
+      self.optimizer.zero_grad()
       # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
       # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
 
       # Predict
-      y_pred = model(data)
+      y_pred = self.model(data)
 
       # Calculate loss
-      loss = F.nll_loss(y_pred, target)
+      loss = self.loss_func(y_pred, target)
 
       # L2 loss
 
       # L1 loss
-      l1 = 0
+      loss_l1 = 0
       # lambda_l1 = 0.05
-      for p in model.parameters():
-        l1 = l1 + p.abs().sum()
-      loss = loss + lambda_l1*l1
+	  if lambda_l1 > 0:
+		  for p in self.model.parameters():
+			loss_l1 = loss_l1 + p.abs().sum()
+		  loss = loss + lambda_l1*loss_l1
 
       self.train_losses.append(loss)
 
       # Backpropagation
       loss.backward()
-      optimizer.step()
+      self.optimizer.step()
 
       # Learning rate for onecycle LR # Vamsi - added
       # scheduler.step()
@@ -60,43 +76,43 @@ class Trainer():
       pbar.set_description(desc= f'Train set: Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
       self.train_acc.append(100*correct/processed)
 
-  def test(self, model, device, test_loader):
-      model.eval()
+  def test(self):
+      self.model.eval()
       test_loss = 0
       correct = 0
       with torch.no_grad():
-          for data, target in test_loader:
-              data, target = data.to(device), target.to(device)
-              output = model(data)
+          for data, target in self.test_loader:
+              data, target = data.to(self.device), target.to(self.device)
+              output = self.model(data)
               test_loss += F.nll_loss(output, target, reduction='sum').item()  # sum up batch loss
               pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
               correct += pred.eq(target.view_as(pred)).sum().item()
 
-      test_loss /= len(test_loader.dataset)
+      test_loss /= len(self.test_loader.dataset)
       self.test_losses.append(test_loss)
 
       print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-          test_loss, correct, len(test_loader.dataset),
-          100. * correct / len(test_loader.dataset)))
+          test_loss, correct, len(self.test_loader.dataset),
+          100. * correct / len(self.test_loader.dataset)))
       
-      self.test_acc.append(100. * correct / len(test_loader.dataset))
+      self.test_acc.append(100. * correct / len(self.test_loader.dataset))
 
   def getValues(self):
     return (self.train_losses, self.test_losses, self.train_acc, self.test_acc)
 
-  def get_misclassified(self, model, test_loader, device):
+  def get_misclassified(self):
     misclassified = []
     misclassified_pred = []
     misclassified_target = []
     # put the model to evaluation mode
-    model.eval()
+    self.model.eval()
     # turn off gradients
     with torch.no_grad():
-        for data, target in test_loader:
+        for data, target in self.test_loader:
           # move them to the respective device
-          data, target = data.to(device), target.to(device)
+          data, target = data.to(self.device), target.to(self.device)
           # do inferencing
-          output = model(data)
+          output = self.model(data)
           # get the predicted output
           pred = output.argmax(dim=1, keepdim=True)
 
