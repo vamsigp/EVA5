0b66deaf0e4e1c6a23eb9d60812d9b466119cdcb
Vamsi <vkrishna84@gmail.com>
2020-09-26 23:55:48 +0530

    fixed inconsistencies in model
    fixed inconsistencies in model

--- trainer/models/QuizDNN.py
+++ trainer/models/QuizDNN.py
@@ -11,11 +11,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class QuizDNN(nn.Module):
-	def __init__(self, args):
-		super(QuizDNN, self).__init__()
-		
-		
-		self.convblock1 = nn.Sequential(
+    def __init__(self, args):
+        super(QuizDNN, self).__init__()
+        
+        
+        self.convblock1 = nn.Sequential(
             nn.Conv2d(3, 32, kernel_size=(1, 1), padding=0, bias=False),
             nn.BatchNorm2d(32),
             nn.ReLU(),
@@ -92,30 +92,30 @@ class QuizDNN(nn.Module):
         self.convblock11 = nn.Sequential(
             nn.Conv2d(128, 10, kernel_size=(1, 1), bias=False),
         )
-		
-	
-	def forward(self, x):
-		x1 = self.convblock1(x) # 1x1 conv
-		x2 = self.convblock2(x1)
-		x3 = self.convblock3(x1+x2)
-		x4 = self.pool1(x1+x2+x3)
-		
-		x4 = self.convblock4(x4) # 1x1 conv
-		x5 = self.convblock5(x4)
-		x6 = self.convblock6(x4 + x5)
-		x7 = self.convblock6(x4+x5+x6)
-		x8 = self.pool2(x5+x6+x7)
-		
-		x8 = self.convblock7(x8) # 1x1 conv
-		x9 = self.convblock8(x8)
-		x10 = self.convblock9(x8 + x9)
-		x11 = self.convblock10(x8 + x9 + x10)
-		
-		x12 = self.gap(x11)
-		x13 = self.convblock11(x12) # 1x1 conv
-		
-		x13 = x13.view(-1, 10)
-		
-		return x13
-		
-		
+        
+    
+    def forward(self, x):
+        x1 = self.convblock1(x) # 1x1 conv
+        x2 = self.convblock2(x1)
+        x3 = self.convblock3(x1+x2)
+        x4 = self.pool1(x1+x2+x3)
+        
+        x4 = self.convblock4(x4) # 1x1 conv
+        x5 = self.convblock5(x4)
+        x6 = self.convblock6(x4 + x5)
+        x7 = self.convblock6(x4+x5+x6)
+        x8 = self.pool2(x5+x6+x7)
+        
+        x8 = self.convblock7(x8) # 1x1 conv
+        x9 = self.convblock8(x8)
+        x10 = self.convblock9(x8 + x9)
+        x11 = self.convblock10(x8 + x9 + x10)
+        
+        x12 = self.gap(x11)
+        x13 = self.convblock11(x12) # 1x1 conv
+        
+        x13 = x13.view(-1, 10)
+        
+        return x13
+        
+        
